c3b604590c9a81fd3b3873e4a7ce5ea0bbf261fd
Maya <mayadelzer@gmail.com>
2022-06-25 23:06:44 -0700

    Add the rest of files
    Added the rest of Basic_HTML_and_JavaScript_1 folder:
    - Edited Html_1
    - Added HTML 2 (for some reason button does nothing. The code is copied exactly as the example so I don't know why)
    - Added JavaScript 1
    Added Basic_HTML_and_CSS folder:
    - Basic HTML 3

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Red Alert, Red Alert!");
+funtion My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	String;
+}
